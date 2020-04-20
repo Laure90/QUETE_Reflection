a1f818784f08f1919ebf76ec516a9764558131b4
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-04-20 10:49:32 +0200

    Add files via upload

---
+++ ReflectedClass1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QUETE_Reflection
+{
+    public class ReflectedClass1
+    {
+        private int _id;
+        private string _name { get; set; }
+
+        public void MakeId()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                _id = i;
+                _name = "John";
+                Console.WriteLine(_id + _name);
+            }
+        }
+    }
+}
