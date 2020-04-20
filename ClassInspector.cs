7a9cfa6523725d46f3b6e3239b64674534e6f278
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-04-20 10:57:08 +0200

    Add files via upload

---
+++ ClassInspector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace QUETE_Reflection
+{
+    public class ClassInspector
+    {
+        public static void GetAllProperties(object reflected)
+        {
+            Type objectType = reflected.GetType();
+
+            PropertyInfo[] properties = objectType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Console.WriteLine("Propreties : ");
+            foreach (var element in properties)
+            {
+                Console.WriteLine("\t" + element);
+            }
+        }
+
+        public static void GetAllFields(object reflected)
+        {
+            Type objectType = reflected.GetType();
+
+            FieldInfo[] fieldInfos = objectType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Console.WriteLine("Fields : ");
+            foreach (var element in fieldInfos)
+            {
+                Console.WriteLine("\t" + element);
+            }
+        }
+
+        public static void GetAllMethods(object reflected)
+        {
+            Type objectType = reflected.GetType();
+
+            MethodInfo[] methods = objectType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Console.WriteLine("Methods : ");
+            foreach (var element in methods)
+            {
+                Console.WriteLine("\t" + element);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace QUETE_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informations of the ReflectedClass1 :");
+            var reflected = new ReflectedClass1();
+            ClassInspector.GetAllProperties(reflected);
+            ClassInspector.GetAllFields(reflected);
+            ClassInspector.GetAllMethods(reflected);
+
+            Console.WriteLine("\nInformations of the ReflectedClass2 :");
+            var reflected2 = new ReflectedClass2();
+            ClassInspector.GetAllProperties(reflected2);
+            ClassInspector.GetAllFields(reflected2);
+            ClassInspector.GetAllMethods(reflected2);
+        }
+    }
+}
